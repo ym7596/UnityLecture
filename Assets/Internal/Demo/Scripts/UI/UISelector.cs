8c429510ef0b4308f568b3c1568c2f0c745e0314
YONGMIN <ym7596@naver.com>
2024-07-15 23:43:22 +0900

    feat : icon add

---
+++ Assets/Internal/Demo/Scripts/UI/UISelector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISelector : MonoBehaviour
+{
+    [SerializeField] private CategorySO _categorySO;
+
+    [SerializeField] private SpaceObjectDataSO _spaceObjectDataSO;
+
+    [SerializeField] private Transform _categoryRoot;
+    [SerializeField] private Transform _itemcellViewRoot;
+    void Start()
+    {
+        
+    }
+
+    
+}
